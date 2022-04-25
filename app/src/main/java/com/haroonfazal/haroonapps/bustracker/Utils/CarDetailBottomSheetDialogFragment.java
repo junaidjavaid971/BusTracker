aab8600a65b62a1f14813bb9b0ef84de4773f52b
Ali <muhammadali90995@gmail.com>
2022-04-25 13:58:40 +0500

    Car dialog added

---
+++ app/src/main/java/com/haroonfazal/haroonapps/bustracker/Utils/CarDetailBottomSheetDialogFragment.java
@@ -0,0 +1,56 @@
+package com.haroonfazal.haroonapps.bustracker.Utils;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.databinding.DataBindingUtil;
+
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.haroonfazal.haroonapps.bustracker.R;
+import com.haroonfazal.haroonapps.bustracker.databinding.CarBottomSheetLayoutBinding;
+
+public class CarDetailBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+    CarBottomSheetLayoutBinding binding;
+    Marker marker;
+
+    public CarDetailBottomSheetDialogFragment(Marker marker) {
+        this.marker = marker;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.car_bottom_sheet_layout, container, false);
+        return binding.getRoot();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        View bottomSheet = (View) view.getParent();
+        bottomSheet.setBackgroundTintMode(PorterDuff.Mode.CLEAR);
+        bottomSheet.setBackgroundTintList(ColorStateList.valueOf(Color.TRANSPARENT));
+        bottomSheet.setBackgroundColor(Color.TRANSPARENT);
+
+        binding.tvDriverName.setText(marker.getTitle());
+        binding.tvBusNumber.setText(marker.getSnippet());
+        binding.ivDownArrow.setOnClickListener(view1 -> {
+            getFragmentManager().beginTransaction()
+                    .remove(CarDetailBottomSheetDialogFragment.this).commit();
+        });
+
+
+    }
+}
